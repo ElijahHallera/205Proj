8c27c771b6fe2c578539ff2c254e5f9deafd8146
Jonathan-Welham <jwelham@csumb.edu>
2020-11-30 18:18:04 -0800

    API working

--- app.py
+++ app.py
@@ -1,5 +1,31 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+import json
+import requests
+from prettyprinter import pprint
+from urllib.request import urlopen
+import ssl
+
+key='ddoOH0QwXpcTGp5Hsyv8kxYDbdheTri8sDBp36qX'
+
+# ssl._create_default_https_context = ssl._create_unverified_context
+# my_site='https://api.nasa.gov/planetary/apod?api_key=' + key
+# site_html=urlopen(my_site)
+# print(site_html.read())
+
+payload = {
+    'api_key': key,
+    'start_date': '2020-05-01',
+    'end_date': '2020-05-01'
+}
+
+endpoint = 'https://api.nasa.gov/planetary/apod'
+try:
+    r = requests.get(endpoint, params=payload)
+    data = r.json()
+    pprint(data)
+except:
+    print('please try again')
 
 app = Flask(__name__)
 Bootstrap(app)
